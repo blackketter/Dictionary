f068b6a01117ea986919c26032313e868736c383
Dean Blackketter <dean@blackketter.com>
2018-04-08 19:09:00 -0700

    Checkpoint refactoring.  Dictionary is broken but EEPROMDictionary still works.

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
 #include "Dictionary.h"
 
-// todo: handle error condition when prefs are too big
+typedef uint8_t datasize_t;
+
+Dictionary::~Dictionary() {
+  free(dictData);
+}
+
+void Dictionary::begin(uint8_t initVersion) {
+  version = initVersion;
+}
 
 // entries are saved:
 //   tag string (zero terminated), length byte, data bytes (of length given before)
 
-bool Dictionary::write(tag_t tag, size_t size, const uint8_t* data) {
+bool Dictionary::set(tag_t tag, size_t size, const uint8_t* data) {
+
+  if (size > MAX_ENTRY_SIZE) { return false; }
 
   bool changed = false;
   size_t offset = findTag(tag);
@@ -19,9 +29,11 @@ bool Dictionary::write(tag_t tag, size_t size, const uint8_t* data) {
       writeTag(offset, tag, size, data);
       changed = true;
     } else {
-      if (((int)remaining() - oldDataSize + size) < 0) {
-        // not enough room to write
-        return false;
+      if ((size-oldDataSize) > remaining()) {
+        if (!resize(used() - oldDataSize + size)) {
+          // not enough room to write
+          return false;
+        }
       }
 
       // delete pref if the new size is not the same as the existing one
@@ -30,9 +42,13 @@ bool Dictionary::write(tag_t tag, size_t size, const uint8_t* data) {
       changed = true;
     }
   } else {
-    if (((int)remaining() - size) < 0) {
-      // not enough room to write
-      return false;
+    // need space for tag + nul char + one byte of size info + the actual data
+    size_t spaceNeeded = strlen(tag) + 1 + sizeof(datasize_t) + size;
+    if (spaceNeeded > remaining()) {
+      if (!resize(used() + spaceNeeded)) {
+        // not enough room to write
+        return false;
+      }
     }
   }
   // if it isn't already written and it has a size, we'll tack it on to the end
@@ -51,6 +67,10 @@ bool Dictionary::write(tag_t tag, size_t size, const uint8_t* data) {
   return true;
 }
 
+bool Dictionary::set(String& tag, String& data) {
+  return set(tag.c_str(), data.length()+1, (const uint8_t*)data.c_str());
+}
+
 size_t Dictionary::size(tag_t tag) {
   size_t offset = findTag(tag);
 
@@ -61,16 +81,16 @@ size_t Dictionary::size(tag_t tag) {
   }
 }
 
-size_t Dictionary::read(tag_t tag, size_t size, uint8_t* data) {
+size_t Dictionary::get(tag_t tag, size_t size, uint8_t* data) {
 
   size_t offset = findTag(tag);
 
   if (offset) {
     offset += strlen(tag) + 1;
-    size_t readSize = prefsData[offset];
+    size_t readSize = dictData[offset];
     offset++;
     readSize = min(readSize, size);  // if the sizes don't match, choose the smaller
-    memcpy(data, &prefsData[offset], readSize);
+    memcpy(data, &dictData[offset], readSize);
 
     return readSize;
   } else {
@@ -78,17 +98,13 @@ size_t Dictionary::read(tag_t tag, size_t size, uint8_t* data) {
   }
 }
 
-bool Dictionary::write(String& tag, String& data) {
-  return write(tag.c_str(), data.length()+1, (const uint8_t*)data.c_str());
-}
-
-size_t Dictionary::read(String& tag, String& data) {
+size_t Dictionary::get(String& tag, String& data) {
   size_t offset = findTag(tag.c_str());
 
   if (offset) {
     offset += tag.length() + 1;
     offset++;
-    data = (const char*) (&prefsData[offset]);
+    data = (const char*) (&dictData[offset]);
 
     return data.length();
   } else {
@@ -114,36 +130,42 @@ void Dictionary::deleteTag(size_t start) {
   size_t len = end - nextTag + tagSize(end);
 
   if (nextTag <= end) {
-    memmove(prefsData+start, prefsData+nextTag, len);
+    memmove(dictData+start, dictData+nextTag, len);
   }
 
 }
 
 size_t Dictionary::tagSize(size_t offset) {
   size_t endoffset = offset;
-  endoffset += strlen((const char*)&prefsData[endoffset]) + 1;
-  endoffset += prefsData[endoffset];
+  if (dictData == nullptr) { return 0; }
+  endoffset += strlen((const char*)&dictData[endoffset]) + 1;
+  endoffset += dictData[endoffset];
   endoffset++;
 
   return endoffset - offset;
 }
 
+
 size_t Dictionary::tagDataSize(size_t offset) {
   size_t endoffset = offset;
-  endoffset += strlen((const char*)&prefsData[endoffset]) + 1;
-  return prefsData[endoffset];
+  if (dictData == nullptr) { return 0; }
+  endoffset += strlen((const char*)&dictData[endoffset]) + 1;
+  return dictData[endoffset];
 }
 
+// returns offset of data for a given tag, returns zero if not found
 size_t Dictionary::findTag(tag_t tag) {
+  if (dictData == nullptr) { return 0; }
+
   size_t offset = 0;
-  while (offset < totalPrefsSize) {
-    if (strcmp(tag, (const char*)&prefsData[offset]) == 0) {
+  while (offset < dictDataSize) {
+    if (strcmp(tag, (const char*)&dictData[offset]) == 0) {
       return offset;
-    } else if (strcmp(endTag, (const char*)&prefsData[offset]) == 0) {
+    } else if (strcmp(endTag, (const char*)&dictData[offset]) == 0) {
       return 0;  // hit the end tag, so it's not found
     } else {
-      offset += strlen((const char*)&prefsData[offset]) + 1;
-      offset += prefsData[offset] + 1;
+      offset += strlen((const char*)&dictData[offset]) + 1;
+      offset += dictData[offset] + 1;
     }
   }
 
@@ -152,31 +174,38 @@ size_t Dictionary::findTag(tag_t tag) {
   return 0;
 }
 
+// total amount of dictData used
 size_t Dictionary::used() {
+  if (dictData == nullptr) { return 0; }
   size_t offset = findTag(endTag);
   return offset + tagSize(offset);
 }
 
 size_t Dictionary::writeTag(size_t offset, tag_t tag, size_t size, const uint8_t* data) {
-    if (size > MAX_TAG_SIZE) {
+    if (size > MAX_ENTRY_SIZE) {
       return 0;
     }
-    strcpy((char*)&prefsData[offset], tag);
+    strcpy((char*)&dictData[offset], tag);
     offset += strlen(tag) + 1;
 
-    prefsData[offset] = size;
+    dictData[offset] = size;
     offset++;
 
-    memcpy(&prefsData[offset], data, size);
+    memcpy(&dictData[offset], data, size);
     offset+= size;
 
     return offset;
 }
 
 void Dictionary::reset() {
-    // initial version tag is missing, clear out prefs
+    // initial version tag is missing, clear out data
     size_t offset = 0;
-    // write a version tag & 1 byte version
+
+    free(dictData);
+    dictData = nullptr;
+    dictDataSize = 0;
+
+    // write a version tag
     offset = writeTag(offset, versionTag, sizeof(version), &version);
 
     // write an end tag with the same 1 byte version
@@ -184,26 +213,45 @@ void Dictionary::reset() {
     save();
 }
 
+bool Dictionary::resize(size_t newSize) {
+  uint8_t* newData = (uint8_t*)realloc((void*)dictData, newSize);
+
+  if (newData) {
+    dictData = newData;
+    dictDataSize = newSize;
+  }
+
+  return newData != nullptr;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// EEPROMDictionary
+
+EEPROMDictionary::EEPROMDictionary() {
+  dictDataSize = EEPROM_SIZE;
+  dictData = EEPromData;
+}
+
 void EEPROMDictionary::save() {
   // this is brute force:
   // todo - only write out the bytes that have changed
-  for (size_t i = 0; i < totalPrefsSize; i++) {
-    EEPROM.write(i, prefsData[i]);
+  for (size_t i = 0; i < dictDataSize; i++) {
+    EEPROM.write(i, dictData[i]);
   }
 }
 
-void EEPROMDictionary::load(uint8_t initVersion) {
+void EEPROMDictionary::begin(uint8_t initVersion) {
   version = initVersion;
 
   // todo - only read in the bytes that have been used
-  for (size_t i = 0; i < totalPrefsSize; i++) {
-    prefsData[i] = EEPROM.read(i);
+  for (size_t i = 0; i < dictDataSize; i++) {
+    dictData[i] = EEPROM.read(i);
   }
 
-  // check if prefs have been initialized
-  if (strcmp((const char*)versionTag, (const char*)prefsData) != 0 ||
-      prefsData[strlen(versionTag) + 1] != sizeof(version) ||
-      prefsData[strlen(versionTag) + 2] != version) {
+  // check if Dict have been initialized
+  if (strcmp((const char*)versionTag, (const char*)dictData) != 0 ||
+      dictData[strlen(versionTag) + 1] != sizeof(version) ||
+      dictData[strlen(versionTag) + 2] != version) {
     reset();
   }
 }
--- Dictionary.h
+++ Dictionary.h
@@ -1,5 +1,5 @@
-#ifndef _Preferences_
-#define _Preferences_
+#ifndef _Dictionary_
+#define _Dictionary_
 
 #include "EEPROM.h"
 #include "WString.h"
@@ -8,38 +8,45 @@ typedef const char* tag_t;
 
 class Dictionary {
   public:
+    ~Dictionary();
+    virtual void begin(uint8_t initVersion = 1);
 
-    bool write(tag_t tag, size_t size, const uint8_t* data);  // return true on success
-    size_t read(tag_t tag, size_t size, uint8_t* data);
-    size_t size(tag_t tag);
+    bool set(tag_t tag, size_t size, const uint8_t* data);  // return true on success
+    bool set(String& tag, String& data);
 
-    bool write(String& tag, String& data);
-    size_t read(String& tag, String& data);
+    size_t get(tag_t tag, size_t size, uint8_t* data);
+    size_t get(String& tag, String& data);
 
-    void reset();  // danger: this erases all the prefs
+    void reset();  // danger: this erases all the data
 
     uint8_t getVersion() { return version; }
-    size_t used();  // returns total amount of space used
-    size_t size() { return totalPrefsSize; }  // returns total size of prefs space
-    size_t remaining() { return size() - used(); }
-    static constexpr size_t MAX_TAG_SIZE = 255;
 
-    const uint8_t* data() { return prefsData; }  // for debugging only
+    size_t size(tag_t tag); // size of a given tag, returns 0 if it doesn't exist
+    size_t size() { return dictDataSize; }  // returns total size of dict memory allocations
+    size_t used();  // returns total amount of space used
+
+    size_t remaining() { return size() - used(); }
+
+    static constexpr size_t MAX_ENTRY_SIZE = 255;
+
+    const uint8_t* data() { return dictData; }  // for debugging only
 
   protected:
-    size_t totalPrefsSize = 0;
-    uint8_t* prefsData = nullptr;
-    virtual void save() {};
-
+    size_t dictDataSize = 0;
+    uint8_t* dictData = nullptr;
     uint8_t version;
-    static constexpr tag_t versionTag = "vers";
-    static constexpr tag_t endTag = "end";
 
-    size_t writeTag(size_t offset, tag_t tag, size_t size, const uint8_t* data);
+    virtual void save() {};
+    virtual bool resize(size_t newSize);  // returns false if resizing fails
+
     size_t findTag(tag_t tag);
+    size_t writeTag(size_t offset, tag_t tag, size_t size, const uint8_t* data);
     size_t tagDataSize(size_t offset);
     void deleteTag(size_t start);
     size_t tagSize(size_t offset);
+
+    static constexpr tag_t versionTag = "vers";
+    static constexpr tag_t endTag = "end";
 };
 
 #if TEENSY
@@ -50,12 +57,14 @@ class Dictionary {
 
 class EEPROMDictionary : public Dictionary {
   public:
-    EEPROMDictionary() { totalPrefsSize = EEPROM_SIZE; prefsData = EEPromData; }
-    void load(uint8_t initVersion = 1);  // 1 - default version
+    EEPROMDictionary();
+    virtual void begin(uint8_t initVersion = 1);  // 1 - default version
 
   protected:
+    virtual bool resize(size_t newSize) { return false; }  // we can't resize the flash area
     virtual void save();
 
+  private:
     uint8_t EEPromData[EEPROM_SIZE];  // this keeps a full copy of the flash in RAM, which is wasteful, but avoids complications of using FlexRAM directly
 };
 
